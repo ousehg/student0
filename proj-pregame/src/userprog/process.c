303f89cc152bafe5b4f3a6b4f7f70ca836fcdbd2
huzhi <hu.zhi@outlook.com>
2023-02-12 06:11:12 +0000

    Submit proj-pregame, fix page fault of do-nothing & stack-align-0

--- proj-pregame/src/userprog/process.c
+++ proj-pregame/src/userprog/process.c
@@ -100,6 +100,7 @@ static void start_process(void* file_name_) {
     if_.cs = SEL_UCSEG;
     if_.eflags = FLAG_IF | FLAG_MBS;
     success = load(file_name, &if_.eip, &if_.esp);
+    if_.esp -= 20;
   }
 
   /* Handle failure with succesful PCB malloc. Must free the PCB */
